a9ea8b877a04a810bf55f6329cfa95f717968596
hugosinp <hugo.anaponn@gmail.com>
2022-01-21 16:38:14 +0100

    added contact form submit

--- src/app/components/organisms/oContactSection.js
+++ src/app/components/organisms/oContactSection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Box,
     Button,
@@ -17,14 +17,62 @@ import {
     useClipboard,
     useColorModeValue,
     VStack,
-    Container
+    Container,
+    useToast,
+    Select
 } from '@chakra-ui/react';
 import { BsGithub, BsLinkedin, BsPerson } from 'react-icons/bs';
 import { MdEmail, MdOutlineEmail } from 'react-icons/md';
-  
+
+import emailjs from 'emailjs-com';
+
 const OContactSection = () => {
     
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [inquiry, setInquiry] = useState('')
+    const [message, setMessage] = useState('')
+
+    const [loading, setLoading] = useState(false)
+
+    const toast = useToast()
+
     const { hasCopied, onCopy } = useClipboard('hugo.sinprasith@gmail.com');
+
+    const sendEmail = (name, email, inquiry, message) => {
+
+        setLoading(true)
+        emailjs.send('service_2tt70n9', 'template_pqraf6b', {
+            name: name,
+            email: email,
+            inquiry: inquiry,
+            message: message,
+        }, 'user_OTQoaChpT6sgokhhmzCe0')
+        .then((result) => {
+            setLoading(false)
+            return (
+                toast({
+                        title: 'Message sent !',
+                        description: "I'll answer you as possible ! 🚀",
+                        status: 'success',
+                        duration: 5000,
+                        isClosable: true,
+                })
+            )
+                                    
+        }, (error) => {
+            setLoading(false)
+            return (
+                toast({
+                    title: 'Oops there was an error...',
+                    description: "You may try again later or contact me directly via email or social medias !",
+                    status: 'error',
+                    duration: 9000,
+                    isClosable: true,
+                })
+            )
+        });
+    }
 
     return (
         <Container maxW={'4xl'} py={{ base: 35, md: 30 }} centerContent>
@@ -38,6 +86,8 @@ const OContactSection = () => {
                     }}>
                         Let's talk !
                     </Heading>
+
+                    <Heading></Heading>
 
                     <Stack w={'full'} spacing={{ base: 4, md: 8, lg: 20 }} p={5} direction={{ base: 'column', md: 'row' }}>
                         <Stack align="center" justify="space-around" direction={{ base: 'row', md: 'column' }}>
@@ -77,30 +127,37 @@ const OContactSection = () => {
 
                         <Box w={'full'} bg={useColorModeValue('white', 'gray.700')} borderRadius="lg" p={8} color={useColorModeValue('gray.700', 'whiteAlpha.900')} shadow="base">
                             <VStack spacing={5}>
-                                <FormControl isRequired>
+                                <FormControl onChange={(e) => setName(e.target.value)} isRequired>
                                     <FormLabel>Name</FormLabel>
                                     <InputGroup>
                                         <InputLeftElement zIndex={0} children={<BsPerson />} />
                                         <Input type="text" name="name" placeholder="Your Name" />
                                     </InputGroup>
                                 </FormControl>
-
-                                <FormControl isRequired>
+                                <FormControl onChange={(e) => setEmail(e.target.value)} isRequired>
                                     <FormLabel>Email</FormLabel>
-
                                     <InputGroup>
                                         <InputLeftElement zIndex={0} children={<MdOutlineEmail />} />
                                         <Input
                                             type="email"
                                             name="email"
                                             placeholder="Your Email"
+                                            zIndex={0}
                                         />
                                     </InputGroup>
                                 </FormControl>
 
-                                <FormControl isRequired>
-                                    <FormLabel>Message</FormLabel>
+                                <FormControl onChange={(e) => setInquiry(e.target.value)} isRequired>
+                                    <FormLabel>Type of inquiry</FormLabel>
+                                    <Select id='inquiry'>
+                                        <option>Business</option>
+                                        <option>Collaboration</option>
+                                        <option>Other</option>
+                                    </Select>
+                                </FormControl>
 
+                                <FormControl onChange={(e) => setMessage(e.target.value)} isRequired>
+                                    <FormLabel>Message</FormLabel>
                                     <Textarea
                                         name="message"
                                         placeholder="Your Message"
@@ -108,17 +165,36 @@ const OContactSection = () => {
                                         resize="none"
                                     />
                                 </FormControl>
-
-                                <Button
-                                    colorScheme="blue"
-                                    bg="blue.400"
-                                    color="white"
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}
-                                    isFullWidth>
-                                        Send Message
-                                </Button>
+                                {
+                                    loading ?
+                                        <Button
+                                            colorScheme="blue"
+                                            bg="blue.400"
+                                            color="white"
+                                            _hover={{
+                                                bg: 'blue.500',
+                                            }}
+                                            isLoading
+                                            loadingText='Submitting'
+                                            isFullWidth>
+                                            Send Message
+                                        </Button>
+                                    :
+                                        <Button
+                                            colorScheme="blue"
+                                            bg="blue.400"
+                                            color="white"
+                                            _hover={{
+                                                bg: 'blue.500',
+                                            }}
+                                            onClick={() => {
+                                                sendEmail(name, email, inquiry, message)
+                                            }}
+                                            isFullWidth>
+                                            Send Message
+                                        </Button>
+                                }
+                                
                             </VStack>
                         </Box>
                     </Stack>
