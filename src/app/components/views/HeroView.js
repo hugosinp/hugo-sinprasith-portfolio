23ce20e680fb27b645f3f0f1967876b09c879f81
hugosinp <hugo.anaponn@gmail.com>
2022-01-26 17:57:05 +0100

    modified bio section and added hero animation

--- src/app/components/views/HeroView.js
+++ src/app/components/views/HeroView.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
 
 import { Container, Flex } from '@chakra-ui/react'
 
 import OHeroSection from '../organisms/oHeroSection';
 
-const HeroView = ({ id }) => {
+const HeroView = ({ id, fadeIn }) => {
+
+    const {ref, inView} = useInView({ threshold: 0.1 })
+    const animation = useAnimation()
+
+    useEffect(() => {
+        fadeIn(animation, inView)
+    }, [fadeIn, animation, inView])
+
     return (
-        <Container maxW="container.xl" p={0}  id={id}>
-            <Flex height={{ base: '100vh' }} align={'center'}>
-                <OHeroSection />
-            </Flex>
-        </Container>
+        <div id={id}>
+            <motion.div animate={animation}>
+                <Container maxW="container.xl" p={0} ref={ref}>
+                    <Flex height={{ base: '100vh' }} align={'center'}>
+                        <OHeroSection />
+                    </Flex>
+                </Container>
+            </motion.div>
+        </div>
     )
 }
 
