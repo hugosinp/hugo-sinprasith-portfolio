7d7f541199f3b2b023182615eeb883f5b7edfd2b
hugosinp <hugo.anaponn@gmail.com>
2022-09-13 17:26:45 +0200

    added blur circles

--- src/app/components/organisms/oHeroSection.js
+++ src/app/components/organisms/oHeroSection.js
@@ -4,20 +4,17 @@ import { Box, Heading, Container, Text, Stack, Flex } from '@chakra-ui/react';
 
 const OHeroSection = () => {
 	return (
-		<Container maxW={'4xl'} py={{ base: 30, lg: 40 }} centerContent>
-			<Flex h={{ base: 'auto' }} direction={{ base: 'column', lg: 'row' }} align={'center'}>
-				<Stack as={Box} textAlign={{ base: 'center', lg: 'start' }}>
+		<Container maxW={'4xl'} py={{ base: 30, lg: 40 }} centerContent zIndex={2}>
+			<Flex h={{ base: 'auto' }} pt={{ base: 0, lg: 20}} direction={{ base: 'column', lg: 'column' }} align={'center'}>
+				<Stack as={Box} textAlign={{ base: 'center', lg: 'center' }}>
 					<Heading fontWeight={'bold'} fontSize={{ base: '4xl', sm: '4xl', md: '6xl' }} lineHeight={'110%'}>
 						Hugo Sinprasith
 					</Heading>
 					<Text fontSize={'xl'} as={'span'} color={'blue.300'}>
-						<Text as="span" fontWeight={'bold'} bgClip="text" bgGradient="linear(to-r, #4DB8FF, #19A3FF)">
-							Full-stack Developer
-						</Text>{' '}
-						based in Paris.
+						Full-stack Developer based in Paris.
 					</Text>
 				</Stack>
-				<Stack mt={{ base: 10 }}>
+				<Stack mt={{ base: 8 }} width={'60%'}>
 					<lottie-player
 						src="https://assets1.lottiefiles.com/packages/lf20_bufblleg.json"
 						background="transparent"
